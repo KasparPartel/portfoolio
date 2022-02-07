893b2966ecb38d811453bafb44c328cd8bfee0ab
Kaspar J. Pärtel <kasparjohannes@gmail.com>
2022-02-07 21:18:18 +0200

    finished responsive navbar without animations

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,16 +1,24 @@
-import React from "react";
-import "./Nav.css"
+import React, { useState } from "react";
+import "./Nav.css";
+import { FaBars } from "react-icons/fa";
 
 const Nav = () => {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
+
   return (
-    <nav className="nav">
-      <h2 className="nav__heading"><a href="#home" >Kaspar J. Pärtel</a></h2>
+    <nav className={hamburgerOpen ? "nav nav__active" : "nav"}>
+      <h2 className="nav__heading"><a href="#home">KJP</a></h2>
       <ol>
         <li className="nav__list-item"><a href="Home">Home</a></li>
         <li className="nav__list-item"><a href="#about">About</a></li>
         <li className="nav__list-item"><a href="#work">Work</a></li>
         <li className="nav__list-item"><a href="#contact">Contact</a></li>
       </ol>
+      <FaBars className="nav__hamburger" size="1.5em" onClick={toggleHamburger} />
     </nav>
   );
 };
